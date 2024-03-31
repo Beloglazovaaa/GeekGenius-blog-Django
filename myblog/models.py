d7f028dce184bcf4949ccddd93381d59c83491ed
Beloglazovaaa <Beloglazova2004@mail.ru>
2024-03-31 18:16:10 +0300

    docker-compose added, models and settings fixes

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.SlugField()
+    description = models.TextField()
+    content = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
