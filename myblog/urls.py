a3330eb00b14616df3dc6d4940e9da3a74f6fe42
Beloglazovaaa <Beloglazova2004@mail.ru>
2024-04-02 01:46:45 +0300

    added posts, read more and home buttons

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, PostDetailView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-
+from .models import Post
+from django.http import Http404
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog/index.html'
-        )
+        posts = Post.objects.all()
+        return render(request, 'myblog/index.html', context={
+            'posts': posts
+        })
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={
+            'post': post
+        })
