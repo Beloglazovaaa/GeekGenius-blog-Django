2b174f0e56a3f4c35e3f4057f30705c50e819f2f
Beloglazovaaa <Beloglazova2004@mail.ru>
2024-04-03 00:13:06 +0300

    registration added

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MainView, PostDetailView
+from .views import MainView, PostDetailView, SignUpView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
     path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from .models import Post
-from django.http import Http404
 from django.core.paginator import Paginator
+from .models import Post
+from .forms import SignUpForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
@@ -24,3 +26,20 @@ class PostDetailView(View):
             'post': post
         })
 
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
