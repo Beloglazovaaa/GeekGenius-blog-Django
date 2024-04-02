5365c12f9d480dd88c7cf9fbfe3237322fb210c7
Beloglazovaaa <Beloglazova2004@mail.ru>
2024-04-02 17:06:56 +0300

    pagination and translator added

--- myblog/views.py
+++ myblog/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all()
+        posts = Post.objects.all().order_by('-created_at')
+        paginator = Paginator(posts, 3)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         return render(request, 'myblog/index.html', context={
-            'posts': posts
+            'page_obj': page_obj
         })
 
 
@@ -17,3 +23,4 @@ class PostDetailView(View):
         return render(request, 'myblog/post_detail.html', context={
             'post': post
         })
+
