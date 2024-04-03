99378f2ece2e743700e0b359d481cd389e615140
Beloglazovaaa <Beloglazova2004@mail.ru>
2024-04-03 23:55:21 +0300

    modal windows added :)

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import MainView, PostDetailView, SignUpView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
     path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
 from .forms import SignUpForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.views import View
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
@@ -43,3 +45,22 @@ class SignUpView(View):
         return render(request, 'myblog/signup.html', context={
             'form': form,
         })
+
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        form = AuthenticationForm()
+        return render(request, 'myblog/login.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                form.add_error(None, "Неверное имя пользователя или пароль")
+        return render(request, 'myblog/login.html', {'form': form})
