968d2d6e37cd86e6c90e95ae4dd22ceafb0c32b5
Beloglazovaaa <Beloglazova2004@mail.ru>
2024-04-04 21:34:15 +0300

    blog section button (without urls), signin, signup and signout added

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import MainView, PostDetailView, SignUpView
 from django.contrib.auth import views as auth_views
+from .views import SignInView
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
 
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
     path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('signin/', SignInView.as_view(), name='signin'),
+    path('signout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='signout'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
-from .forms import SignUpForm
+from .forms import SignUpForm, SignInForm
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import AuthenticationForm
@@ -47,20 +47,25 @@ class SignUpView(View):
         })
 
 
-class LoginView(View):
+class SignInView(View):
     def get(self, request, *args, **kwargs):
-        form = AuthenticationForm()
-        return render(request, 'myblog/login.html', {'form': form})
+        form = SignInForm()
+        return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
 
     def post(self, request, *args, **kwargs):
-        form = AuthenticationForm(request, data=request.POST)
+        form = SignInForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return HttpResponseRedirect('/')
             else:
-                form.add_error(None, "Неверное имя пользователя или пароль")
-        return render(request, 'myblog/login.html', {'form': form})
+                form.add_error(None, "Неправильный пароль или указанная учётная запись не существует!")
+                return render(request, "myblog/signin.html", {"form": form})
+        return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
